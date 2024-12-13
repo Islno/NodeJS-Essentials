4a4d70786c5cc3b65d49753458b42ffac5732a76
islno <islaniopbl@gmail.com>
2024-12-13 16:01:20 -0300

    .

--- arquivo.js
+++ arquivo.js
@@ -1,5 +1,13 @@
 const path = require('node: path');
-
+const fs = require('node: fs');
 const filepath = path.join(process.cwd(),'texto.txt' )
 
 console.log(filepath) // /home/runner/curso-nodejs/texto.txt
+
+fs.readFile(filepath,{},(erro,dados)=>{
+if (erro){
+    console.log('erro')
+    return
+}
+console.log(dados)
+})
